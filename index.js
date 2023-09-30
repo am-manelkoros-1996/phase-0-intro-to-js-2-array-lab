fd876839842d0cd8eea6ffee88372b497d2502d4
moringa <emmanuel.kipngeno@student.moringaschool.com>
2023-09-30 14:56:06 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution here!
+ // Write your solution here!
+ let cats = ["Milo", "Otis", "Garfield"];
+
+ function destructivelyAppendCat(name) {
+     cats.push(name);
+     }
+     function destructivelyPrependCat(name) {
+         cats.unshift(name);
+         }
+         function destructivelyRemoveLastCat(name) {
+             let lastCat = cats.pop();
+             return lastCat;
+ 
+     }
+     destructivelyRemoveFirstCat(); {
+     let firstCat = cats.shift();
+ };
+ 
